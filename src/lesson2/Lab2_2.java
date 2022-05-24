f5bc83ff5fff0979617a88f1a4df23ec1fe4178e
thuannt <thuannt@mobio.vn>
2022-05-24 16:26:40 +0700

    done lab 2_2

--- src/lesson2/Lab2_2.java
+++ src/lesson2/Lab2_2.java
@@ -11,11 +11,11 @@ public class Lab2_2 {
         remainder = enterNumber%2;
         if(remainder == 0)
         {
-            System.out.println(" Your number "+enterNumber+" is Even! ");
+            System.out.println(" Your number "+enterNumber+" is an even number! ");
         }
         else
         {
-            System.out.println(" Your number "+enterNumber+" is Odd! ");
+            System.out.println(" Your number "+enterNumber+" is an odd number! ");
         }
     }
 }
