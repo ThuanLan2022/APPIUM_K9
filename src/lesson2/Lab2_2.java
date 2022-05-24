8aa11f295ec1ac650d9f2e3d7cbb63bee1ca1413
thuannt <thuannt@mobio.vn>
2022-05-24 16:22:10 +0700

    done lab 2_2

---
+++ src/lesson2/Lab2_2.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Lab2_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int enterNumber, remainder;
+        System.out.print(" Please enter your number: ");
+        enterNumber = scanner.nextInt();
+        remainder = enterNumber%2;
+        if(remainder == 0)
+        {
+            System.out.println(" Your number "+enterNumber+" is Even! ");
+        }
+        else
+        {
+            System.out.println(" Your number "+enterNumber+" is Odd! ");
+        }
+    }
+}
