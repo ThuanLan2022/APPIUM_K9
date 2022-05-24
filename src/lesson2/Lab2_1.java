c42851f4a4ed56301db2f09d7ed92ed7a40071e0
thuannt <thuannt@mobio.vn>
2022-05-24 15:17:55 +0700

    done lab 21

--- src/lesson2/Lab2_1.java
+++ src/lesson2/Lab2_1.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Lab2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float myWeight, myHeight,BMI;
+        float yourWeight, yourHeight,BMI;
 
-        System.out.print(" Please enter your height: ");
-        myHeight = scanner.nextFloat();
         System.out.print(" Please enter your weight: ");
-        myWeight = scanner.nextFloat();
-        BMI = myWeight / (myHeight * 2);
+        yourWeight = scanner.nextFloat();
+        System.out.print(" Please enter your height: ");
+        yourHeight = scanner.nextFloat();
+        if(yourHeight==0){
+            System.out.print("\nYour height must be non-zero");
+            System.out.println("\nPlease re-enter your height: ");
+            yourHeight = scanner.nextFloat();
+        }
+        BMI =  yourWeight/ (yourHeight * 2);
         System.out.printf(" Your BMI: %.1f", BMI);
         if (BMI < 18.5){
             System.out.println(" => Underweight");
