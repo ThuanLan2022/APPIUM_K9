dc90e3faca9297c5dac94c096b8047cd4d0fe44e
thuannt <thuannt@mobio.vn>
2022-06-02 14:26:28 +0700

    done lab 2_1

--- src/lesson2/Lab2_2.java
+++ src/lesson2/Lab2_2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Lab2_2 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int enterNumber, remainder;
         System.out.print(" Please enter an integer number: ");
