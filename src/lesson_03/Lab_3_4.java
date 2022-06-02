5b812d4ed9d14ee0ae493c3f401d87a30783c1ab
thuannt <thuannt@mobio.vn>
2022-06-02 18:19:17 +0700

    done lesson3

--- src/lesson_03/Lab_3_4.java
+++ src/lesson_03/Lab_3_4.java
@@ -4,44 +4,46 @@ public class Lab_3_4 {
     public static void main(String[] args) {
         int[] intArr1 = {12, 34, 1, 16, 28};
         int[] intArr2 = {1, 12, 16, 28, 34};
-        int [] newArr = null;
+        int[] newArr = null;
         System.out.println("======Merge 2 SORTED integer array into one SORTED array======");
         newArr = merge(intArr1, intArr2);
         int leng2Arr = intArr1.length + intArr2.length;
-        for (int a; a< newArr.length; a++ ){
+        for (int i = 0; i < newArr.length; i++) {
 
-            System.out.println("Mang moi " + newArr[i] );
+            System.out.println("Mang moi " + newArr[i]);
         }
     }
-    public static int[] merge(int[] a, int[] b) {
-        int aIndex = a.length - 1;
-        int bIndex = b.length - 1;
-        int cIndex = a.length + b.length - 1;
-        int[] c = new int[cIndex + 1];
+
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int aIndex = arr1.length - 1;
+        int bIndex = arr2.length - 1;
+        int cIndex = arr1.length + arr2.length - 1;
+        int[] arrAll = new int[cIndex + 1];
 
         // sắp xếp các mảng theo thứ tự tăng dần
-        sortASC(a);
-        sortASC(b);
+        sortASC(arr1);
+        sortASC(arr2);
 
-        // trộn mảng a và b thành c
+        // trộn mảng arr1 và arr2 thành arrAll
         for (int i = cIndex; i > -1; i--) {
             if (aIndex > -1 && bIndex > -1) {
-                if (a[aIndex] > b[bIndex]) {
-                    c[i] = a[aIndex--];
+                if (arr1[aIndex] > arr2[bIndex]) {
+                    arrAll[i] = arr1[aIndex--];
                 } else {
-                    c[i] = b[bIndex--];
+                    arrAll[i] = arr2[bIndex--];
                 }
             } else if (bIndex == -1) {
-                c[i] = a[aIndex--];
+                arrAll[i] = arr1[aIndex--];
             } else if (aIndex == -1) {
-                c[i] = b[bIndex--];
+                arrAll[i] = arr2[bIndex--];
             }
         }
-        return c;
+        return arrAll;
     }
-    public static void sortASC(int [] arr) {
+
+    public static void sortASC(int[] arr) {
         int temp = arr[0];
-        for (int i = 0 ; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     temp = arr[j];
