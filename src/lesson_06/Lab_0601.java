9e526141b6a372ef4f749ecda382327d8179bf40
thuannt <thuannt@mobio.vn>
2022-06-14 15:08:20 +0700

    done lab 6

--- src/lesson_06/Lab_0601.java
+++ src/lesson_06/Lab_0601.java
@@ -4,9 +4,7 @@ public class Lab_0601 {
     public static void main(String[] args) {
         String inputString = "2hrs and 5 minutes";
         String hourNumber = String.valueOf(inputString.trim().charAt(0));
-        System.out.println(hourNumber);
         String minuteNumber = String.valueOf(inputString.trim().charAt(9));
-        System.out.println(minuteNumber);
         int intHour = Integer.valueOf(hourNumber) * 60;
         int intMinute = Integer.valueOf(minuteNumber);
         int total = intHour + intMinute;
