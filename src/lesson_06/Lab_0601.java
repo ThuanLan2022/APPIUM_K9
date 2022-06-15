6819cb3adcd64c2156b01eb25c70f8088ec53c54
thuannt <thuannt@mobio.vn>
2022-06-15 14:10:31 +0700

    done lab 6

--- src/lesson_06/Lab_0601.java
+++ src/lesson_06/Lab_0601.java
@@ -3,8 +3,10 @@ package lesson_06;
 public class Lab_0601 {
     public static void main(String[] args) {
         String inputString = "2hrs and 5 minutes";
-        String hourNumber = String.valueOf(inputString.trim().charAt(0));
-        String minuteNumber = String.valueOf(inputString.trim().charAt(9));
+        int indexHour = inputString.indexOf("hrs");
+        int indexMinutes = inputString.indexOf("minutes");
+        String hourNumber = String.valueOf(inputString.trim().charAt(indexHour-1));
+        String minuteNumber = String.valueOf(inputString.trim().charAt(indexMinutes - 2));
         int intHour = Integer.valueOf(hourNumber) * 60;
         int intMinute = Integer.valueOf(minuteNumber);
         System.out.printf("Total minutes is: %d (Minutes)", (intHour + intMinute));
